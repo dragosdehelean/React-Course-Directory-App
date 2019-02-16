c88d450ce5666fc922db5c42440e0a8f938a16a6
Dragos Dehelean <dragos.dehelean@selenis.ro>
2019-02-16 15:33:52 +0200

    Updates

---
+++ precache-manifest.b2de89ef1185c586c4151cd5707f9a51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e33933b0f3c5ef12e7e",
+    "url": "/React-Course-Directory-App/static/js/runtime~main.1e33933b.js"
+  },
+  {
+    "revision": "374fe56ec88ddebbbcfb",
+    "url": "/React-Course-Directory-App/static/js/main.374fe56e.chunk.js"
+  },
+  {
+    "revision": "053c2164a0117b1ffdb2",
+    "url": "/React-Course-Directory-App/static/js/2.053c2164.chunk.js"
+  },
+  {
+    "revision": "374fe56ec88ddebbbcfb",
+    "url": "/React-Course-Directory-App/static/css/main.0dca12b8.chunk.css"
+  },
+  {
+    "revision": "7f3ce011efcd05a37df10f36ce7067f6",
+    "url": "/React-Course-Directory-App/index.html"
+  }
+];
