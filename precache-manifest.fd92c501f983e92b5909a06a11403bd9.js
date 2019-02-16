12a820c11c2999d65ede727383f4923efb92993e
Dragos Dehelean <dragos.dehelean@selenis.ro>
2019-02-16 15:37:29 +0200

    Updates

--- precache-manifest.fd92c501f983e92b5909a06a11403bd9.js
+++ precache-manifest.fd92c501f983e92b5909a06a11403bd9.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/React-Course-Directory-App/static/js/runtime~main.1e33933b.js"
   },
   {
-    "revision": "374fe56ec88ddebbbcfb",
-    "url": "/React-Course-Directory-App/static/js/main.374fe56e.chunk.js"
+    "revision": "f51f8f1cd88add642ba4",
+    "url": "/React-Course-Directory-App/static/js/main.f51f8f1c.chunk.js"
   },
   {
     "revision": "053c2164a0117b1ffdb2",
     "url": "/React-Course-Directory-App/static/js/2.053c2164.chunk.js"
   },
   {
-    "revision": "374fe56ec88ddebbbcfb",
+    "revision": "f51f8f1cd88add642ba4",
     "url": "/React-Course-Directory-App/static/css/main.0dca12b8.chunk.css"
   },
   {
-    "revision": "7f3ce011efcd05a37df10f36ce7067f6",
+    "revision": "94f58426fd785859ae1369b214b7f74e",
     "url": "/React-Course-Directory-App/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Course-Directory-App/precache-manifest.b2de89ef1185c586c4151cd5707f9a51.js"
+  "/React-Course-Directory-App/precache-manifest.fd92c501f983e92b5909a06a11403bd9.js"
 );
 
 workbox.clientsClaim();
